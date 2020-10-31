6573a5db96bbf4eea318c2bf7634a83f922533cb
SaxyCat <67085313+SaxyCat@users.noreply.github.com>
2020-10-31 23:37:15 +0800

    Update settings.py

--- Reddinfo/settings.py
+++ Reddinfo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0c$$0!1)v%)=a3)omfkd478$3=)!oj90s@h8rl_=jkj$&ch=q-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
